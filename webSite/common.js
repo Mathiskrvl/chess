0346caf955e9a3cd621a5b56ac58ee1c1f58f549
Raphala27 <raphaelissartial@gmail.com>
2024-01-22 22:12:40 +0100

    add-homeButton + reorganization-of-files

---
+++ webSite/common.js
@@ -0,0 +1,6 @@
+// Here are the common functions for the severals games
+
+function goToHomePage() {
+    // Redirige vers la page d'accueil (index.html)
+    window.location.href = 'index.html';
+  }
