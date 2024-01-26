ec8a5d0e12d666153db7424d72fc384168487d33
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-01-26 15:10:06 +0100

    refactor: :zap: change the structure of main.js et index.js

--- main.js
+++ main.js
@@ -2,16 +2,24 @@ import '@/common.css'
 import '/style.css'
 import '@/common.js'
 
-window.startGame = (mode) => {
-    console.log("testtttt")
-    // Rediriger vers la page du jeu avec le mode sélectionné
+let games = ['tic-tac-toe', 'heads-tails', 'chess'];
+
+// add event listener on games
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('tic-tac-toe').addEventListener('click', () => startGame('tic-tac-toe'));
+    document.getElementById('heads-tails').addEventListener('click', () => startGame('heads-tails'));
+    document.getElementById('chess').addEventListener('click', () => startGame('chess'));
+});
+
+// function redirect vers la page du jeu avec le mode sélectionné
+function startGame(mode) {
     if (mode === 'tic-tac-toe') {
         window.location.href = '/tic-tac-toe/';
     } 
-    else if (mode === 'heads-or-tails') {
+    else if (mode === 'heads-tails') {
         window.location.href = '/heads-tails/';
     }
     else if (mode === 'chess') {
         window.location.href = '/chess/';
     }
-  }
+}
