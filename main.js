cfa8f50ae5aebe1a08ba18e4ec9df3bd76ce2b25
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-01-26 15:33:10 +0100

    perf: :zap:

--- main.js
+++ main.js
@@ -4,14 +4,9 @@ import '@/common.js'
 
 let games = ['tic-tac-toe', 'heads-tails', 'chess'];
 
-// Ajouter des écouteurs d'événements pour chaque jeu
+// Ajouter des écouteurs d'événements pour chaque jeu pour qu'il redirect vers le bon jeu
 document.addEventListener('DOMContentLoaded', () => {
     games.forEach(game => {
-        document.getElementById(game).addEventListener('click', () => startGame(game));
+        document.getElementById(game).addEventListener('click', () => window.location.href = `/${game}/`);
     });
 });
-
-// Fonction pour rediriger vers la page du jeu avec le mode sélectionné
-function startGame(mode) {
-    window.location.href = `/${mode}/`;
-}
