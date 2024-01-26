91df4cf48c8bd413cedca41504153e4fac99afa2
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-01-26 15:22:12 +0100

    perf: :zap:

--- main.js
+++ main.js
@@ -4,22 +4,14 @@ import '@/common.js'
 
 let games = ['tic-tac-toe', 'heads-tails', 'chess'];
 
-// add event listener on games
+// Ajouter des écouteurs d'événements pour chaque jeu
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('tic-tac-toe').addEventListener('click', () => startGame('tic-tac-toe'));
-    document.getElementById('heads-tails').addEventListener('click', () => startGame('heads-tails'));
-    document.getElementById('chess').addEventListener('click', () => startGame('chess'));
+    games.forEach(game => {
+        document.getElementById(game).addEventListener('click', () => startGame(game));
+    });
 });
 
-// function redirect vers la page du jeu avec le mode sélectionné
+// Fonction pour rediriger vers la page du jeu avec le mode sélectionné
 function startGame(mode) {
-    if (mode === 'tic-tac-toe') {
-        window.location.href = '/tic-tac-toe/';
-    } 
-    else if (mode === 'heads-tails') {
-        window.location.href = '/heads-tails/';
-    }
-    else if (mode === 'chess') {
-        window.location.href = '/chess/';
-    }
+    window.location.href = `/${mode}/`;
 }
