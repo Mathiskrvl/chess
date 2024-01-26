e7e680e9302eb7b388483e77ae222177d32fbb34
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-01-26 18:21:31 +0100

    fix: :rocket: github page static

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-    resolve : {
-        alias: {
-            '@': path.resolve(__dirname, './common'),
-        }
-    },
-    build: {
-        rollupOptions: {
-          base: '/chess/', // Laisser avec github actions
-          input: {
-            main: path.resolve(__dirname, "index.html"),
-            chess: path.resolve(__dirname, "chess/index.html"),
-            tit_tac_toe: path.resolve(__dirname, "tic-tac-toe/index.html"),
-            heads_tails: path.resolve(__dirname, "heads-tails/index.html"),
-          }
-        }
+  base: '/chess/', // Laisser avec github actions
+  resolve : {
+    alias: {
+        '@': path.resolve(__dirname, './common'),
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        chess: path.resolve(__dirname, "chess/index.html"),
+        tit_tac_toe: path.resolve(__dirname, "tic-tac-toe/index.html"),
+        heads_tails: path.resolve(__dirname, "heads-tails/index.html"),
       }
-    });
+    }
+  }
+});
