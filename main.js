4d6132c346ea6686d6db5fc83fdb4c8d04c5a287
Mathis KERAVAL <mkeraval@mediametrie.fr>
2024-01-30 15:31:42 +0100

    fix: :zap: lien a href better

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 import '@/common.css'
 import '/style.css'
 import '@/auth.js'
-
-let games = ['tic-tac-toe', 'heads-tails', 'chess'];
-
-// Ajouter des écouteurs d'événements pour chaque jeu pour qu'il redirect vers le bon jeu
-document.addEventListener('DOMContentLoaded', () => {
-    games.forEach(game => {
-        document.getElementById(game).addEventListener('click', () => window.location.href = `/${game}/`);
-    });
-});
